de5b7cf942d22fad7fddc47876971ea6e57e567c
Arian Botine <arian.botine@shift.com.br>
2022-12-03 10:01:45 -0300

    Buscar elementos no DOM utilizando debugElement

--- src/app/shared/directives/action/action.directive.spec.ts
+++ src/app/shared/directives/action/action.directive.spec.ts
@@ -1,6 +1,7 @@
 import { componentFactoryName } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ActionDirective } from './action.directive';
 import { ActionDirectiveModule } from './action.module';
 
@@ -19,10 +20,32 @@ describe(ActionDirective.name, () => {
   })
 
   it('(D) (@Output appAction) should emit event with payload when ENTER key is pressed', () => {
-    const divElement: HTMLElement = fixture.nativeElement.querySelector('.dummy-component');
+    //const divElement: HTMLElement = fixture.nativeElement.querySelector('.dummy-component');
+    const divElement: HTMLElement = fixture.debugElement.query(By.directive(ActionDirective)).nativeElement;
     const event = new KeyboardEvent('keyup', { key: "Enter" });
     divElement.dispatchEvent(event);
-    expect(component.hasEvent()).toBe(true);
+    expect(component.hasEvent()).toBeTrue();
+  });
+
+  it('(D) (@Output appAction) should emit event with payload when clicked', () => {
+    //const divElement: HTMLElement = fixture.nativeElement.querySelector('.dummy-component');
+    const divElement: HTMLElement = fixture.debugElement.query(By.directive(ActionDirective)).nativeElement;
+    const event = new Event('click');
+    divElement.dispatchEvent(event);
+    expect(component.hasEvent()).toBeTrue();
+  });
+
+  it('(D) (@Output appAction) should emit event with payload when clicked or ENTER key is pressed', () => {
+    //const divElement: HTMLElement = fixture.nativeElement.querySelector('.dummy-component');
+    const divElement: HTMLElement = fixture.debugElement.query(By.directive(ActionDirective)).nativeElement;
+    const eventClick = new Event('click');
+    const eventKeyboard = new KeyboardEvent('keyup', { key: "Enter" });
+    divElement.dispatchEvent(eventClick);
+    expect(component.hasEvent()).withContext('Click event').toBeTrue();
+    component.resetForNewExpectation()
+    divElement.dispatchEvent(eventKeyboard);
+    expect(component.hasEvent()).withContext('Keyboard event "keyup Enter"').toBeTrue();
+
   });
 });
 
@@ -31,11 +54,16 @@ describe(ActionDirective.name, () => {
 })
 class ActionDirectiveTestComponent {
   private event: Event = null;
+
   public actionHandler(event: Event): void {
     this.event = event;
   }
 
   public hasEvent(): boolean {
     return !!this.event;
+  }
+
+  public resetForNewExpectation(): void {
+    return this.event = null;
   }
 }
