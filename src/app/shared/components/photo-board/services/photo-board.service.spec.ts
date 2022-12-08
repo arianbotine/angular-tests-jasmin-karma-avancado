4593cdf7413e47ac5ff2397b7049474e79b597de
Arian Botine <arian.botine@shift.com.br>
2022-12-07 23:17:12 -0300

    Iniciando configuração para a utilização do HttpClientTestingModule

---
+++ src/app/shared/components/photo-board/services/photo-board.service.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { PhotoBoardService } from './photo-board.service';
+
+describe(PhotoBoardService.name, () => {
+  let service: PhotoBoardService;
+  let httpController: HttpTestingController;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PhotoBoardService],
+    });
+    service = TestBed.inject(PhotoBoardService);
+    httpController = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
