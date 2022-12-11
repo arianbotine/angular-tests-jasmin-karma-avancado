b77aa6a411c20460c255dc4946a436bb77f70a50
Arian Botine <arian.botine@shift.com.br>
2022-12-11 20:56:42 -0300

    Finalizando implementação de mock provider usando useValue

--- src/app/shared/components/photo-list/photo-list-component-mock-provider.spec.ts
+++ src/app/shared/components/photo-list/photo-list-component-mock-provider.spec.ts
@@ -1,3 +1,4 @@
+import { PhotoBoardMockService } from './../photo-board/services/mocks/photo-board-mock.service';
 import { PhotoBoardService } from './../photo-board/services/photo-board.service';
 import { HttpClientModule } from '@angular/common/http';
 import { PhotoListModule } from './photo-list.module';
@@ -5,54 +6,43 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PhotoListComponent } from './photo-list.component';
 import { buildPhotoList } from '../photo-board/test/build-photo-list';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
+import { Photo } from '../photo-board/interfaces/photo';
 
 describe(PhotoListComponent.name, () => {
-    let component: PhotoListComponent;
-    let fixture: ComponentFixture<PhotoListComponent>;
-    let service: PhotoBoardService;
+  let component: PhotoListComponent;
+  let fixture: ComponentFixture<PhotoListComponent>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [PhotoListModule, HttpClientModule],
-            providers: [{
-                provide: PhotoBoardService,
-                useValue: {
-                    getPhotos() {
-                        return buildPhotoList()
-                    }
-                }
-            }]
-        }).compileComponents();
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PhotoListModule, HttpClientModule],
+      providers: [
+        {
+          provide: PhotoBoardService,
+          useValue: {
+            getPhotos(): Observable<Photo[]> {
+              return of(buildPhotoList());
+            },
+          },
+        },
+      ],
+    }).compileComponents();
+  });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(PhotoListComponent);
-        component = fixture.componentInstance;
-        service = TestBed.inject(PhotoBoardService);
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PhotoListComponent);
+    component = fixture.componentInstance;
+  });
 
-    it('Should create component', () => {
-        expect(component).toBeTruthy();
-    });
+  it('Should create component', () => {
+    expect(component).toBeTruthy();
+  });
 
-    it('(D) Should display board when data arrives', () => {
-        const photos = buildPhotoList();
-        spyOn(service, 'getPhotos').and.returnValue(of(photos));
-        fixture.detectChanges();
-        const board = fixture.nativeElement.querySelector('app-photo-board');
-        const loader = fixture.nativeElement.querySelector('.loader');
-        expect(board).withContext('Should display board').not.toBeNull();
-        expect(loader).withContext('Should not display loader').toBeNull();
-    });
-
-    it('(D) Should display board when data loading', () => {
-        const photos = buildPhotoList();
-        spyOn(service, 'getPhotos').and.returnValue(null);
-        fixture.detectChanges();
-        const board = fixture.nativeElement.querySelector('app-photo-board');
-        const loader = fixture.nativeElement.querySelector('.loader');
-        expect(board).withContext('Should not display board').toBeNull();
-        expect(loader).withContext('Should display loader').not.toBeNull();
-    });
+  it('(D) Should display board when data arrives', () => {
+    fixture.detectChanges();
+    const board = fixture.nativeElement.querySelector('app-photo-board');
+    const loader = fixture.nativeElement.querySelector('.loader');
+    expect(board).withContext('Should display board').not.toBeNull();
+    expect(loader).withContext('Should not display loader').toBeNull();
+  });
 });
